44b8acba7637da683c7eac569e1331f93b2bc95e
Lee Clark <lee.clark@tracsis.com>
2024-10-02 20:37:13 +0100

    added workflow

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    // You can add custom rules here
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
